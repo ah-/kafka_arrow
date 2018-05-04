c09016f2c94b56a14d4f4aab4302cebf2be3e506
iztok <kardenal.mendoza@gmail.com>
2018-05-04 14:08:36 +0100

    fix a hard-coded paths in setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,40 @@
-
-
 import sys
 #from distutils.core import setup, Extension
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 import pyarrow
+import itertools as it
 
 requires=["setuptools", "cython", "numpy", "pyarrow"]
 
-extra_compile_args = ['-std=c++11']
-aux_lib_dirs = []
+# c/c++ compiler params
+name = 'kafka_arrow'
+srcs = ['kafka_arrow.pyx']
+compiler_lang = 'c++'
+compiler_args = ['-std=c++11']
+inc_dirs = [np.get_include(), pyarrow.get_include()]
+lib_dirs = [pyarrow.get_library_dirs()]
+libs = [pyarrow.get_libraries(), ['serdes']]
 
-if sys.platform == 'darwin':
-    #extra_compile_args.append('-mmacosx-version-min=10.9')
-    extra_compile_args.append('-stdlib=libc++')
-if sys.platform == 'linux':
-    extra_compile_args.append('-I/opt/python/miniconda3/envs/dud/lib/python3.6/site-packages/pyarrow/include')
-    aux_lib_dirs = ['/opt/python/miniconda3/envs/dud/lib/python3.6/site-packages/pyarrow/']
+# flatten
+libs = list(it.chain.from_iterable(libs))
+lib_dirs = list(it.chain.from_iterable(lib_dirs))
 
-
-print(pyarrow.get_include())
-    
 setup(
     name='kafka_arrow_py',
-    version='0.1.0.1',
+    version='0.1.0.3',
     author='ah',
     author_email='andreas@heider.io',
     url='https://github.com/ah-/kafka_arrow',
     license='Apache 2.0',
     install_requires=requires,
     setup_requires=requires,
-    ext_modules = cythonize(Extension('kafka_arrow', sources=['kafka_arrow.pyx'],
-                                      libraries=['arrow_python', 'serdes'],
-                                      library_dirs=aux_lib_dirs,
-                                      language="c++", extra_compile_args = extra_compile_args)),
-    include_dirs = [np.get_include()],
+    ext_modules = cythonize(Extension(name,
+                                      sources=srcs,
+                                      libraries=libs,
+                                      library_dirs=lib_dirs,
+                                      language=compiler_lang,
+                                      extra_compile_args = compiler_args)),
+    include_dirs = inc_dirs
 )
